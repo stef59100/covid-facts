db86263e136ba51dfcc47e389c38d90897ca34c9
Stef59100 <stephane.joos@gmail.com>
2020-06-03 15:02:19 +0200

    deuxieme commit

--- src/components/global/index.jsx
+++ src/components/global/index.jsx
@@ -7,27 +7,27 @@ const GlobalDataComponent = (props) => {
         <div>
             <h2>Données journalières mondiales </h2>
             <div className="row">
-                <div className="item col justify-content-between align-items-center flex-column odd">
+                <div className="item  col-sm-2 justify-content-between align-items-centerflex-column odd">
                     <h3>Nouveaux cas confirmés</h3>
                     <p>{props.datas.NewConfirmed}</p>
                 </div>
-                <div className="item col justify-content-between align-items-center flex-column even">
+                <div className="item  col-sm-2 justify-content-between align-items-centerflex-column even">
                     <h3>Nombre total de cas confirmés</h3>
                     <p>{props.datas.TotalConfirmed}</p>
                 </div>
-                <div className="item col justify-content-between align-items-center flex-column odd">
+                <div className="item  col-sm-2 justify-content-between align-items-centerflex-column odd">
                     <h3>Nouveaux cas de décès</h3>
                     <p className="align-self-end">{props.datas.NewDeaths}</p>
                 </div>
-                <div className="item col justify-content-between align-items-center flex-column even">
+                <div className="item  col-sm-2 justify-content-between align-items-centerflex-column even">
                 <h3>Nombre total de décès</h3>
                     <p>{props.datas.TotalDeaths}</p>
                 </div>
-                <div className="item col justify-content-between align-items-center flex-column odd">
+                <div className="item  col-sm-2 justify-content-between align-items-centerflex-column odd">
                     <h3>Nouvelles guérisons</h3>
                     <p>{props.datas.NewRecovered}</p>
                 </div>
-                <div className="item col justify-content-between align-items-center flex-column even">
+                <div className="item  col-sm-2 justify-content-between align-items-centerflex-column even">
                     <h3>Nombre total de guérisons</h3>
                     <p>{props.datas.TotalRecovered}</p>
                 </div>
